f843db965a978a5cbcc4f7905114d1e21b284fb4
AashrayAnand <aashray_anand@hotmail.com>
2015-08-13 18:59:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,12 @@ var array2 = new Array(y)
 var a = array.length;
 var b = array2.length;
 var c = b + a;
-document.write("The length of the first array is " + a + "  ");
-document.write(" The length of the second array is " + b + "    ");
+document.write("Final array length: " + c);
 for(var i = 0; i < array.length; i++){
     array[i] = parseInt(prompt("Fill the first array", "1"), 10);
 }
 for(var j = 0; j < array2.length; j++){
     array2[j] = parseInt(prompt("Fill the second array ", "1"), 10);
 }
-document.write(array);
-document.write("," + array2);
-/*var c= [];
-var next;
-while(a.length && b.length){
-        while(array[0]<=array2[0]) c.push(array2.shift());
-        while(array[0]<=array2[0]) c.push(array.shift());
-    }
-    if(a.length) c.push(array);
-    else c.push(array2);
-    return c;
-}*/
+document.write("[" + array);
+document.write("," + array2 + " ]");
